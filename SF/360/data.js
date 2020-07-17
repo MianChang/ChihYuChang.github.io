7cd160a66949019c9fe816838f8ecb195601a4b5
Chih-Yu Chang <67685701+MianChang@users.noreply.github.com>
2020-07-17 13:06:21 +0200

    1

--- SF/360/data.js
+++ SF/360/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-ar-quests",
-      "name": "AR Quests",
+      "id": "0-ansicht",
+      "name": "Ansicht",
       "levels": [
         {
           "tileSize": 256,
@@ -26,10 +26,22 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -3.0951367788451662,
-          "pitch": -0.4956928554850215,
+          "yaw": 0.5953042687723595,
+          "pitch": 0.46092781251273607,
           "rotation": 0,
-          "target": "5-panorama3"
+          "target": "1-foyer"
+        },
+        {
+          "yaw": -0.2624505415662064,
+          "pitch": 0.017118059895816273,
+          "rotation": 0,
+          "target": "5-dach-multifuktionsflaeche"
+        },
+        {
+          "yaw": -0.48981405690432744,
+          "pitch": 0.53329501091061,
+          "rotation": 0,
+          "target": "6-multifuktionsflaeche"
         }
       ],
       "infoHotspots": []
@@ -54,35 +66,42 @@ var APP_DATA = {
       ],
       "faceSize": 1024,
       "initialViewParameters": {
-        "yaw": -1.571674858099401,
         "pitch": 0,
-        "fov": 1.3650016530720892
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": -1.1259533939332087,
-          "pitch": 0.12048527674654963,
-          "rotation": 6.283185307179586,
-          "target": "0-ar-quests"
+          "yaw": -1.1290927757420732,
+          "pitch": 0.12243049840043163,
+          "rotation": 0,
+          "target": "2-quest"
         },
         {
-          "yaw": -3.077759087440059,
-          "pitch": -0.48003204256445997,
+          "yaw": -3.073937623339603,
+          "pitch": -0.5118405024375576,
           "rotation": 0,
-          "target": "3-section"
-        },
-        {
-          "yaw": -2.066953504646161,
-          "pitch": -0.006841950101426519,
-          "rotation": 0,
-          "target": "4-functions"
+          "target": "4-struktur_multifuktionsflaeche"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -0.8004825779784337,
+          "pitch": -0.3156641577371211,
+          "title": "RPB(Role Playing Game)",
+          "text": "Learning system is a RPG system.It ist like a game in wich students assume the roles in a fictional setting."
+        },
+        {
+          "yaw": -2.5100858005656033,
+          "pitch": -0.3366269302166707,
+          "title": "Multi-function glassmonitor",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">the multi-function glassmonitor shows videos or live-videos from other space. Students can also interaction with the other who is in the video.</span>"
+        }
+      ]
     },
     {
-      "id": "2-aussen-fassade",
-      "name": "Aussen Fassade",
+      "id": "2-quest",
+      "name": "Quest",
       "levels": [
         {
           "tileSize": 256,
@@ -106,29 +125,42 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.6340682394292756,
-          "pitch": 0.47519282651702177,
+          "yaw": 2.085604641805018,
+          "pitch": 0.1037033719200906,
           "rotation": 0,
           "target": "1-foyer"
         },
         {
-          "yaw": -0.27953828574285566,
-          "pitch": 0.00004623621406096845,
+          "yaw": -3.0994407916171784,
+          "pitch": -0.36645821982414795,
           "rotation": 0,
-          "target": "6-panorama1"
+          "target": "4-struktur_multifuktionsflaeche"
         },
         {
-          "yaw": -0.01682777170127281,
-          "pitch": 0.5345016726090428,
+          "yaw": -1.3663237722914037,
+          "pitch": -0.03062689141677133,
           "rotation": 0,
-          "target": "3-section"
+          "target": "0-ansicht"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 0.907750594139447,
+          "pitch": -0.024191821465457508,
+          "title": "Teamwork",
+          "text": "The Learning system provides teamwork Quests. By logging into the system displayed students who participate in the Quest at the same time. Learning system increases interaction between students and social skills."
+        },
+        {
+          "yaw": -0.4142877459472789,
+          "pitch": -0.002055359939479473,
+          "title": "AR learning",
+          "text": "Through the leraning system and AR technic extend learning to real life or off-campus space."
+        }
+      ]
     },
     {
-      "id": "3-section",
-      "name": "Section",
+      "id": "3-schnitt_multifuktionsflaeche",
+      "name": "Schnitt_Multifuktionsflaeche",
       "levels": [
         {
           "tileSize": 256,
@@ -146,109 +178,48 @@ var APP_DATA = {
       ],
       "faceSize": 1024,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -0.07647991717997371,
+        "pitch": -0.040260374999526505,
+        "fov": 1.4700985578597092
       },
       "linkHotspots": [
         {
-          "yaw": 2.79797967970614,
-          "pitch": 0.4701699405463984,
+          "yaw": -1.613489397295183,
+          "pitch": -0.5148701894740402,
           "rotation": 0,
-          "target": "2-aussen-fassade"
+          "target": "6-multifuktionsflaeche"
         },
         {
-          "yaw": 1.023696631989857,
-          "pitch": 0.4667565157311113,
-          "rotation": 0,
-          "target": "1-foyer"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-functions",
-      "name": "functions",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -1.1026863102002586,
-          "pitch": -0.47786318639802516,
-          "rotation": 0,
-          "target": "3-section"
-        },
-        {
-          "yaw": 1.1286998967406667,
-          "pitch": 0.19205543687182924,
-          "rotation": 0,
-          "target": "1-foyer"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "5-panorama3",
-      "name": "Panorama3",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.9339912932334791,
-          "pitch": 0.4221483620395894,
+          "yaw": 1.0485236717007993,
+          "pitch": 0.4527943925083342,
           "rotation": 0,
           "target": "1-foyer"
         },
         {
-          "yaw": -2.3678848841614037,
-          "pitch": 0.33169370233051865,
+          "yaw": -2.857704577886178,
+          "pitch": 0.29235706422539387,
           "rotation": 0,
-          "target": "2-aussen-fassade"
+          "target": "0-ansicht"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 0.05851495939902307,
+          "pitch": -0.3166650713167627,
+          "title": "Activity center",
+          "text": "All light in the activity center is controlled. Through projection and monitor can show various possibilities."
+        },
+        {
+          "yaw": 0.11867570850017728,
+          "pitch": 0.05483174486934317,
+          "title": "Functional classrom",
+          "text": "In functional classroom, students learn through physical perception. for example weighlessniss, warm, sound,......"
+        }
+      ]
     },
     {
-      "id": "6-panorama1",
-      "name": "Panorama1",
+      "id": "4-struktur_multifuktionsflaeche",
+      "name": "Struktur_Multifuktionsflaeche",
       "levels": [
         {
           "tileSize": 256,
@@ -272,10 +243,90 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.9252971580500837,
-          "pitch": 0.04828991603116606,
+          "yaw": -1.0605986539888015,
+          "pitch": -0.45490837376669546,
           "rotation": 0,
-          "target": "2-aussen-fassade"
+          "target": "3-schnitt_multifuktionsflaeche"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-dach-multifuktionsflaeche",
+      "name": "Dach-multifuktionsflaeche",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.501975617768167,
+          "pitch": 0.07726566102215138,
+          "rotation": 0,
+          "target": "0-ansicht"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-multifuktionsflaeche",
+      "name": "Multifuktionsflaeche",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.95746248170183,
+          "pitch": 0.48922944285188663,
+          "rotation": 0,
+          "target": "1-foyer"
+        },
+        {
+          "yaw": 1.5037550478887862,
+          "pitch": 0.06637993652377361,
+          "rotation": 0,
+          "target": "0-ansicht"
+        },
+        {
+          "yaw": 0.6740070599254615,
+          "pitch": 0.04883420202444633,
+          "rotation": 0,
+          "target": "3-schnitt_multifuktionsflaeche"
         }
       ],
       "infoHotspots": []
@@ -285,7 +336,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": true,
+    "fullscreenButton": false,
     "viewControlButtons": false
   }
 };
